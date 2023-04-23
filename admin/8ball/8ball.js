6a787dd446484fb503139cb8d2652fdfda18bb27
RhythmGrind <Z001026Z@GMAIL.COM>
2023-04-23 16:56:53 -0700

    u

--- admin/8ball/8ball.js
+++ admin/8ball/8ball.js
@@ -3,29 +3,46 @@ $(document).ready(function(){
     var magic8Ball = {};
     magic8Ball.listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
    
+
+
+
+
     magic8Ball.getAnswer = function(question)
     {
-      var randomNumber = Math.random();
-      var randomAnswer = Math.floor(randomNumber * this.listofanswers.length);
-      var answer = this.listofanswers[randomAnswer];
-      
-      $("#8ball").effect( "shake" );
-      $("#answer").text( answer );
-      $("#answer").fadeIn(3000);
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
-  
-      console.log(question);
-      console.log(answer);
-    };
     $("#answer").hide();
+    $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+    
+    //var randomAnswerIndex = Math.floor(Math.random() * this.listofanswers.length);
   
-    // var onClick = function()
-    // {
-    //   $("#answer").hide();
-    //   $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
-    //   var question = prompt("What do you want to know?");
-    //   magic8Ball.getAnswer(question);
-    // };
+    // Set up the slot machine effect
+    var answerText = "";
+    var counter = 0;
+    var interval = setInterval(function() {
+      // Generate a new random answer every few iterations
+      if (counter % 5 == 0) {
+        var randomAnswerIndex = Math.floor(Math.random() * magic8Ball.listofanswers.length);
+        answerText = magic8Ball.listofanswers[randomAnswerIndex];
+    }
+      
+      // Set the answer text to a random answer
+      //answerText = magic8Ball.listofanswers[randomAnswerIndex];
+      
+      // Update the answer element with the new text
+      $("#answer").text(answerText);
+      
+      // Increment the counter
+      counter++;
+      if (counter >= 50) {
+        clearInterval(interval);
+        $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+        $("#answer").fadeIn(300);
+      }
+    }, 100);
+      //console.log(question);
+      //console.log(answer);
+    };
+
+
     var onClick = function(event) {
         var question = $("#questionBox").val().trim();
         if (question === "") {
